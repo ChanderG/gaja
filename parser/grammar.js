34d5737b8a4d168c88ac70d30949d8da22afaafb
ChanderG <mail@chandergovind.org>
2024-10-03 19:03:02 +0530

    add basic naga grammar and example in ts format

---
+++ parser/grammar.js
@@ -0,0 +1,53 @@
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "naga",
+
+  rules: {
+      module: $ => repeat($._defn),
+
+      _defn: $ => choice(
+          $.func_defn
+      ),
+
+      func_defn: $ => seq(
+          'fun',
+          $.ident,
+          '()',
+          $.block
+      ),
+
+      block: $ => seq(
+          ':',
+          repeat(choice($._expr, $._stmt)),
+          ';'
+      ),
+
+      _expr: $ => choice(
+          $.ident,
+          $.numeral,
+          $.arith_expr,
+      ),
+
+      _stmt: $ => choice(
+          $.assign_stmt,
+      ),
+
+      arith_expr: $ => prec.left(seq(
+          $._expr,
+          choice("+", "-"),
+          $._expr
+      )),
+
+      assign_stmt: $ => seq(
+          $.ident,
+          "=",
+          $._expr
+      ),
+
+      ident: $ => /[a-z]+/,
+      numeral: $ => /\d+/,
+  }
+
+});
