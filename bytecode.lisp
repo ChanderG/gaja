fff67b89fa903a485e2f87cb8ccaeffa6ba669f1
ChanderG <mail@chandergovind.org>
2024-10-05 15:15:25 +0530

    first attempt at serializing to binary

--- bytecode.lisp
+++ bytecode.lisp
@@ -1,3 +1,5 @@
+;;; Code objects
+
 (defclass func-co ()
   ((name :initarg :name :accessor name)
    (instr :initarg :instr :accessor instr)
@@ -13,6 +15,47 @@
     (format out "~s~%" (instr fco))
     (format out "~s~%" (consts fco))
     (format out "~s" (vars fco))))
+
+;;; Serialization formats
+
+(defmacro deftag (name val)
+  `(defconstant ,name ,val))
+
+;; tags from 0-9 reserved for special use later
+
+;; 10-40 for fundamental types
+; short string of length <255, so that the length is represented in a single byte
+(deftag t/short-string 10)
+; next byte refers directly to a single byte char, no length
+(deftag t/char 11)
+; next byte refers to a an int <255; no length
+(deftag t/small-int 12)
+
+;; 40 onwards for custom types
+(deftag t/fco-start 40)
+(deftag t/fco-end 41)
+
+(with-open-file (stream "sample.out"
+                        :direction :output
+                        :if-exists :overwrite
+                        :element-type '(unsigned-byte 8))
+  ;; our special magic string
+  (write-sequence '(#x0b #x89 #x09 #x50) stream)
+  (write-sequence (mapcar 'char-code '(#\g #\a #\j #\a)) stream)
+
+  ;; start writing a function code obj
+  (write-byte t/fco-start stream)
+
+  ;; storing the function name
+  (let* ((name "main"))
+    (write-byte t/short-string stream)
+    (write-byte 4 stream)
+    (write-sequence (map 'list 'char-code name) stream)
+    )
+
+  ;; end func co obj
+  (write-byte t/fco-end stream)
+  )
 
 ; example function code
 (setq ex1
