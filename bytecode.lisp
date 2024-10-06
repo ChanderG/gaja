c6bffda382dd3416cdd81c0e07d34caedc5724de
ChanderG <mail@chandergovind.org>
2024-10-06 09:52:56 +0530

    first attempt at deserializing func co
    update opcode definition space
    add reverse mapping for opcodes

--- bytecode.lisp
+++ bytecode.lisp
@@ -2,9 +2,9 @@
 
 (defclass func-co ()
   ((name :initarg :name :accessor name)
-   (instr :initarg :instr :accessor instr)
-   (consts :initarg :consts :accessor consts)
-   (vars :initarg :vars :accessor vars)))
+   (instr :initarg :instr :accessor instr :initform '())
+   (consts :initarg :consts :accessor consts :initform '())
+   (vars :initarg :vars :accessor vars :initform '())))
 
 (defun make-func-co (name instr consts vars)
   (make-instance 'func-co :name name :instr instr :consts consts :vars vars))
@@ -21,10 +21,33 @@
 (defmacro deftag (name val)
   `(defconstant ,name ,val))
 
-(defmacro defopcode0 (name val)
-  `(defconstant ,name ,val))
-(defmacro defopcode1 (name val)
-  `(defconstant ,name ,val))
+(defconstant opc0-start 127)
+(defconstant opc1-start 190)
+(defvar opc0-curr opc0-start)
+(defvar opc1-curr opc1-start)
+
+;; (setq opc0-curr opc0-start)
+;; (setq opc1-curr opc1-start)
+
+(defvar op-name-mapping (make-array 127))
+
+;; forward mapping is through creation of constants
+;; reverse mapping is an array of symbols
+
+(defmacro defopcode0 (name)
+  `(progn
+     (defconstant ,name (incf opc0-curr))
+     (setf (aref op-name-mapping (- opc0-curr 127))
+        (quote ,name))))
+
+(defmacro defopcode1 (name)
+  `(progn
+     (defconstant ,name (incf opc1-curr))
+     (setf (aref op-name-mapping (- opc1-curr 127))
+        (quote ,name))))
+
+(defun is-opcode1 (val)
+  (and (>= val opc1-start) (<= val opc1-curr)))
 
 ;; tags from 0-9 reserved for special use later
 
@@ -46,13 +69,19 @@
 (deftag t/fco-instr-end 42)
 
 ;; Definition of opcodes
-;; start from 127, just to ensure difference from the tags
-;; strictly not necessary - as long as some special tag values don't clash with opcodes
-(defopcode0 op/print 127)
-(defopcode0 op/add 128)
-(defopcode1 op/load-value 129)
-(defopcode1 op/load-name 130)
-(defopcode1 op/store-name 131)
+(defopcode0 op/print)
+(defopcode0 op/add)
+
+(defopcode1 op/load-value)
+(defopcode1 op/load-name)
+(defopcode1 op/store-name)
+
+(defvar *magic* '())
+
+;; hint: look at the unicode codepoints spelled out
+(setf *magic* (append *magic* '(#x0b #x89 #x09 #x50)))
+;; specific to our vm
+(setf *magic* (append *magic* (mapcar 'char-code '(#\g #\a #\j #\a))))
 
 (defun serialize-co-to-file (fco filename)
   (with-open-file (stream filename
@@ -61,8 +90,7 @@
                           :if-does-not-exist :create
                           :element-type '(unsigned-byte 8))
     ;; our special magic string
-    (write-sequence '(#x0b #x89 #x09 #x50) stream)
-    (write-sequence (mapcar 'char-code '(#\g #\a #\j #\a)) stream)
+    (write-sequence *magic* stream)
 
     ;; start writing a function code obj
     (write-byte t/fco-start stream)
@@ -70,7 +98,7 @@
     ;; storing the function name
     (let* ((name (name fco)))
       (write-byte t/short-string stream)
-      (write-byte 4 stream)
+      (write-byte (length name) stream)
       (write-sequence (map 'list 'char-code name) stream)
       )
 
@@ -94,6 +122,54 @@
     ))
 
 (serialize-co-to-file ex1 "sample.gaja")
+
+(defun deserialize-fco-from-stream (stream &aux
+                                             (fco (make-instance 'func-co)))
+  ;; name of a function
+  (assert (eq (read-byte stream) t/short-string))
+  (let* ((len (read-byte stream))
+         (bytes (make-sequence 'list len)))
+    (read-sequence bytes stream)
+    (setf (name fco) (concatenate 'string (mapcar #'code-char bytes))))
+
+  ;; instructions
+  (loop
+   (let* ((nxt-byte (read-byte stream)))
+     (if (eq nxt-byte t/fco-instr-end)
+         (return)
+         (let* ((opc (aref op-name-mapping (- nxt-byte 127)))
+                (opc-str (symbol-name opc))
+                (new-str (substitute #\- #\/ opc-str :count 1))
+                (opc-new (read-from-string new-str))
+                (opc-list (list opc-new)))
+           (if (is-opcode1 nxt-byte)
+               ;; push the arg in - which means list is reversed
+               (push (read-byte stream) opc-list))
+           (push (reverse opc-list) (instr fco)))
+     )))
+  ;; instruction are added in reverse order, so correct it
+  (setf (instr fco) (reverse (instr fco)))
+
+  fco
+  )
+
+(defun deserialize-co-from-file (filename)
+  (with-open-file (stream filename
+                          :direction :input
+                          :element-type '(unsigned-byte 8))
+
+    (let ((magic (make-sequence 'list 8)))
+      (read-sequence magic stream)
+      (assert (equal magic *magic*)))
+
+    ; expect start of a single fco
+    ; in the future it could be anything else, or repeated
+    (assert (eq (read-byte stream) t/fco-start))
+
+    (deserialize-fco-from-stream stream)
+    ))
+
+(deserialize-co-from-file "sample.gaja")
 
 ; example function code
 (setq ex1
